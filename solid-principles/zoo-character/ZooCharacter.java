bbda78504af422649af6399eb32696681613df33
mahalakshmi-vee <mahalakshmi.veeraraj.scaler@gmail.com>
2024-12-28 01:55:11 -0700

    Add SOLID Principles Implementation in Java
    Implemented the SOLID principles (Single Responsibility, Open/Closed, Liskov Substitution, Interface Segregation, and Dependency Inversion) in Java to improve code maintainability, flexibility, and scalability.

---
+++ solid-principles/zoo-character/ZooCharacter.java
@@ -0,0 +1,37 @@
+package zooCharacter;
+
+public abstract class ZooCharacter {
+	private String name;
+	private int age;
+	private String gender;
+
+	public ZooCharacter(String name) {
+		this.name = name;
+	}
+
+	public abstract void eat();
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+}
