612e7ab14a44417cceede51d8bc758f03f75d396
mahalakshmi-vee <mahalakshmi.veeraraj.scaler@gmail.com>
2024-12-28 01:46:14 -0700

    Add Adapter Pattern in Java
    Implemented the Adapter pattern to allow incompatible interfaces to work together. This enables code reusability and easier integration with existing systems.

---
+++ design-patterns/06-adapter/target/StockXMLDataAnalyzer.java
@@ -0,0 +1,10 @@
+package target;
+
+public class StockXMLDataAnalyzer implements DataAnalyzer {
+	@Override
+	public void analyzeData(String xmlData) {
+		// Logic to perform the data analysis process.
+		System.out.println("Analyzing XML data...");
+		System.out.println(xmlData);
+	}
+}
---
+++ design-patterns/06-adapter/target/StockXMLDataProvider.java
@@ -0,0 +1,8 @@
+package target;
+
+public class StockXMLDataProvider {
+	public String getStockXMLData() {
+		// Example stock XML data.
+		return "<stock><symbol>GOOG</symbol><price>1500</price></stock>";
+	}
+}
