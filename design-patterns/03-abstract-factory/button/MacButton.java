b91118f807f57bd0ce11914a107f69a6ca656f0e
mahalakshmi-vee <mahalakshmi.veeraraj.scaler@gmail.com>
2024-12-28 01:32:40 -0700

    Add Abstract Factory Pattern in Java
    Implemented the Abstract Factory pattern to provide an interface for creating families of related or dependent objects. 
    This allows flexibility in adding new families without changing existing code.

---
+++ design-patterns/03-abstract-factory/button/MacButton.java
@@ -0,0 +1,8 @@
+package button;
+
+public class MacButton implements IButton {
+	@Override
+	public void pressButton() {
+		System.out.println("Mac Button is pressed.");
+	}
+}
---
+++ design-patterns/03-abstract-factory/button/WindowsButton.java
@@ -0,0 +1,8 @@
+package button;
+
+public class WindowsButton implements IButton {
+	@Override
+	public void pressButton() {
+		System.out.println("Windows Button is pressed.");
+	}
+}
