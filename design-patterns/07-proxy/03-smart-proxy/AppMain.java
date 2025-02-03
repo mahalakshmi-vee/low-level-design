fd1a93c46101d829599d641f2c267b3d97e04597
Mahalakshmi Veeraraj <mahalakshmi.veeraraj.scaler@gmail.com>
2025-02-03 14:29:50 +0530

    Add Smart Proxy Pattern in Java

--- design-patterns/07-proxy/03-smart-proxy/AppMain.java
+++ design-patterns/07-proxy/03-smart-proxy/AppMain.java
@@ -1,1 +1,15 @@
+package main;
 
+import dao.ProductDAO;
+import model.Product;
+import proxy.ProductDAOSmartProxy;
+
+public class AppMain {
+	public static void main(String[] args) {
+		ProductDAO productDAO = new ProductDAOSmartProxy();
+		productDAO.create(new Product(1));
+		productDAO.create(new Product(2));
+		productDAO.get(1);
+		productDAO.get(1);
+	}
+}
