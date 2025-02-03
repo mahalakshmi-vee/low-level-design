95fd524ff82d88bec5e96c562bdf6d78ed286750
Mahalakshmi Veeraraj <mahalakshmi.veeraraj.scaler@gmail.com>
2025-02-03 14:20:33 +0530

    Add Virtual Proxy Pattern in Java

--- design-patterns/07-proxy/02-virtual-proxy/AppMain.java
+++ design-patterns/07-proxy/02-virtual-proxy/AppMain.java
@@ -1,1 +1,14 @@
+package main;
 
+import cache.ProductDAOVirtualProxy;
+import model.Product;
+
+public class AppMain {
+	public static void main(String[] args) {
+		ProductDAOVirtualProxy productDAO = new ProductDAOVirtualProxy();
+		productDAO.create(new Product(1));
+		productDAO.create(new Product(2));
+		productDAO.get(1);
+		productDAO.get(1);
+	}
+}
