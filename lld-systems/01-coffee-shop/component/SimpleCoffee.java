cdc9ca05ea2109f21330510fe08794148c7683b6
Mahalakshmi Veeraraj <mahalakshmi.veeraraj.scaler@gmail.com>
2025-02-03 20:57:21 +0530

    feat: implement Coffee Shop System

---
+++ lld-systems/01-coffee-shop/component/SimpleCoffee.java
@@ -0,0 +1,16 @@
+package component;
+
+public class SimpleCoffee implements Coffee {
+
+	@Override
+	public double getCost() {
+
+		return 10;
+	}
+
+	@Override
+	public String getDescription() {
+
+		return "Simple Coffee";
+	}
+}
