9f4e98b596e272802c9888197059c2ee6531f452
Mahalakshmi Veeraraj <mahalakshmi.veeraraj.scaler@gmail.com>
2025-02-17 19:16:37 +0530

    Implement complete LRU Cache system LLD

--- lld-implementations/lru-cache/Node.java
+++ lld-implementations/lru-cache/Node.java
@@ -1,33 +1,20 @@
-class Node {
-        private int key;
-        private int value;
-        private Node next;
-        private Node previous;
-        public Node(int key, int value) {
-            this.key = key;
-            this.value = value;
-            this.next = null;
-            this.previous = null;
-        }
-        public int getKey() {
-            return this.key;
-        }
-        public int getValue() {
-            return this.value;
-        }
-        public Node getNext() {
-            return this.next;
-        }
-        public Node getPrevious() {
-            return this.previous;
-        }
-        public void setValue(int value) {
-            this.value = value;
-        }
-        public void setNext(Node next) {
-            this.next = next;
-        }
-        public void setPrevious(Node previous) {
-            this.previous = previous;
-        }
-    }
+package com.practice.lru_cache;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class Node {
+	private int key;
+	private int value;
+	private Node next;
+	private Node previous;
+
+	public Node(int key, int value) {
+		this.key = key;
+		this.value = value;
+		this.next = null;
+		this.previous = null;
+	}
+}
